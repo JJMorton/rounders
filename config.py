7b4e608e608ec1125b01a66e37b9e89a0a246673
Jack Morton <jjm229@exeter.ac.uk>
2024-05-05 20:44:53 +0100

    Remove old config values

--- config.py
+++ config.py
@@ -3,17 +3,3 @@ DATABASE_FILE = "rounders.db"
 Where to create/find the database, relative to the
 Flask `app.instance_path` directory.
 """
-
-DEFAULT_PAGE_SIZE = 30
-"""
-Number of results returned by each page of an API
-request, unless the `per_page` query parameter is
-given
-"""
-
-MAX_PAGE_SIZE = 100
-"""
-Maximum number of results per page of an API
-request.
-"""
-
